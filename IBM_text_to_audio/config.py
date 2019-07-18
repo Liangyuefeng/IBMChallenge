57df5c77ab91a5b34e23e1ef080261ec54a9f68f
LYF <36235160+Liangyuefeng@users.noreply.github.com>
2019-07-19 00:54:28 +0100

    Update config.py

--- IBM_text_to_audio/config.py
+++ IBM_text_to_audio/config.py
@@ -1,4 +1,4 @@
-iam_apikey = '04Z3vK9211huu2-VdfDxBGll7u90TQn_A1I_z0hjSPlc'
-url = "https://gateway-lon.watsonplatform.net/text-to-speech/api"
+iam_apikey = xxxxxxxxxxxx
+url = xxxxxxxxxxxxxxxxx
 watsonvoice = 'en-US_AllisonVoice'
 
